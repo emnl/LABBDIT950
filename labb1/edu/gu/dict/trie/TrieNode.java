f7367a939cc507e17267d7fefc9d0673a8b8cb68
Hugo Tunius <hugo.tunius@gmail.com>
2012-01-16 21:12:06 +0100

    Added constructors to TrieNode

--- labb1/edu/gu/dict/trie/TrieNode.java
+++ labb1/edu/gu/dict/trie/TrieNode.java
@@ -5,5 +5,24 @@ package edu.gu.dict.trie;
  * @author Hugo Tunius
  */
 public class TrieNode{
+        TrieNode left;
+        TrieNode right;
+        TrieNode parent;
+        
+        public TrieNode(){
+        }
+
+        public TrieNode(TrieNode parent){
+                this.parent = parent;                
+        }
+
+        public TrieNode(TrieNode left, TrieNode right,TrieNode){
+                this.left = left;
+                this.right = right;
+                this.parent = parent;        
+        }
+
+
+
 
 }
